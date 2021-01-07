8949c5c3c05a3a5a99b5be3c291755980200c3f5
Mohamed Ali <mohamedali@aucegypt.edu>
2021-01-07 18:28:49 +0200

    Add add and list APIs

---
+++ AspNetCore.NTier.Web/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.NTier.BussinessLogic;
+using AspNetCore.NTier.Web.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCore.NTier.Web.Controllers
+{
+    [Route("api/[controller]/")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserService userService=new UserService();
+        [HttpGet]
+        [Route("add")]
+        public async Task<bool> AddUser(string firstName,string lastName,string email)
+        {
+            return await userService.AddUser(firstName, lastName, email);
+        }
+        [HttpGet]
+        [Route("list")]
+        public async Task<IEnumerable<UserViewModel>> GetUsers()
+        {
+            var users = await userService.GetUsers();
+            return users.Select(u => 
+            new UserViewModel 
+            { FirstName = u.FirstName, LastName = u.LastName, Email = u.Email }
+            );
+        }
+    }
+}
