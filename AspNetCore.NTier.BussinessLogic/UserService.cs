0f4152a6e8104eb3818ed4eec7571b8e91080240
Mohamed Ali <mohamedali@aucegypt.edu>
2021-01-07 18:14:45 +0200

    Add UserStore and UserService

---
+++ AspNetCore.NTier.BussinessLogic/UserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetCore.NTier.DataAccess.Entities;
+using AspNetCore.NTier.DataAccess.Functions;
+
+namespace AspNetCore.NTier.BussinessLogic
+{
+    public class UserService
+    {
+        UserStore userStore = new UserStore();
+        public async Task<bool> AddUser(string firstName,string lastName,string email)
+        {
+            return await userStore.AddUser(firstName, lastName, email);
+        }
+        public async Task<List<User>> GetUsers()
+        {
+            return await userStore.GetUsers();
+        }
+    }
+}
---
+++ AspNetCore.NTier.DataAccess/Store/UserStore.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AspNetCore.NTier.DataAccess.DataContext;
+using AspNetCore.NTier.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AspNetCore.NTier.DataAccess.Functions
+{
+    public class UserStore
+    {
+        private AppDbContext dbContext = new AppDbContext();
+        
+        public async Task<bool> AddUser(string firstName, string lastName, string email)
+        {
+            try
+            {
+                var user = new User
+                {
+                    FirstName = firstName,
+                    LastName = lastName,
+                    Email = email
+                };
+                await dbContext.Users.AddAsync(user);
+                await dbContext.SaveChangesAsync();
+                return true;
+            }
+            catch (Exception)
+            {
+
+                return false;
+            }
+        }
+        public async Task<List<User>> GetUsers()
+        {
+            return await dbContext.Users.ToListAsync();
+        }
+    }
+}
